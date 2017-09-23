0dda6b3e210db747f50ef312acb26ebcd89dca76
chance <zhoucx@dtdream.com>
2017-09-23 14:00:50 +0800

    Update udpserver.c

--- udpserver.c
+++ udpserver.c
@@ -1,39 +1,67 @@
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <string.h> 
-#include <netinet/in.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#define MAXLINE 80 
-#define SERV_PORT 8888 
-void do_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen) 
-{ 
-int n; 
-socklen_t len; 
-char mesg[MAXLINE]; 
-for(;;) 
-{ 
-len = clilen; 
-n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len); 
-mesg[n]='\0';
-printf("recv msg:%s\n",mesg);
-sendto(sockfd, mesg, n, 0, pcliaddr, len); 
-} 
-} 
-int main(void) 
-{ 
-int sockfd; 
-struct sockaddr_in servaddr, cliaddr; 
-sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
-bzero(&servaddr, sizeof(servaddr)); 
-servaddr.sin_family = AF_INET; 
-servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-servaddr.sin_port = htons(SERV_PORT); 
-if(bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) 
-{ 
-perror("bind error"); 
-exit(1); 
-} 
-do_echo(sockfd, (struct sockaddr *)&cliaddr, sizeof(cliaddr)); 
-return 0; 
-} 
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<errno.h>
+#define RET_OK 0
+#define RET_ERR -1
+#define LISTEN_QUEUE_NUM 5
+#define BUFFER_SIZE 256
+#define ECHO_PORT 9999
+int main(int argc,char **argv)
+{
+	int sockfd,opt=1;
+	uint32_t len;
+	struct sockaddr_in cliaddr;
+	uint8_t buffer[BUFFER_SIZE];
+	int ret=RET_OK;
+	short listen_port=ECHO_PORT;
+        if(argc != 2){
+		printf("usage:%s listen_port.\n",argv[0]);
+		return -1;
+	}
+	listen_port=atoi(argv[1]);
+	if((sockfd=socket(AF_INET,SOCK_DGRAM,0))<0)
+	{
+		perror("ERROR opening socket");
+		return RET_ERR;
+	}
+
+	//SO_REUSEADDR允许重用本地址
+	if((ret=setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt)))<0)
+	{
+		perror("ERROR setsockopt");
+		goto failed;
+	}
+	memset(&cliaddr,0,sizeof(cliaddr));
+	cliaddr.sin_family=AF_INET;
+	cliaddr.sin_addr.s_addr=INADDR_ANY;
+	cliaddr.sin_port=htons(listen_port);
+	if((ret=bind(sockfd,(struct sockaddr*)&cliaddr,sizeof(cliaddr)))<0)
+	{
+		perror("ERROR on binding");
+		goto failed;
+	}
+	printf("udp server listening on *:%d\n", listen_port);
+	do
+	{
+		len=sizeof(cliaddr);
+		if((ret=recvfrom(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*)&cliaddr,&len))>0)
+		{
+			buffer[ret]=0;
+			printf("Recv udp data from %s:%d . length:%d, data:%s\n",
+				inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port), ret,buffer);
+			ret=sendto(sockfd,buffer,ret,0,(struct sockaddr*)&cliaddr,len);
+		}
+	}while(ret>=0);
+failed:
+	close(sockfd);
+	return 0;
+}
